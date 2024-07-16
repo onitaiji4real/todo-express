bc8e533ceb1caddd13291188d7fea3781a19722a
Hank_Chen <hank_chen@rosati.com.tw>
2024-07-16 23:15:54 +0800

    fix: wrong spell

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const todosRouter = require("./routes/todos");
-const cors = require("cores");
+const cors = require("cors");
 const app = express();
 dotenv.config();
 app.use(cors());
